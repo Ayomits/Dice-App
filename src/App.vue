2e1ea89d9282d014b87d82eff1a6fc3456e19e77
unknown <nikitakabo74@gmail.com>
2024-12-22 15:14:27 +0300

    chore: remove vue-router and versel speed insights

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
-  <SpeedInsights>
-    <v-app>
-      <Header />
-      <Rolling />
-    </v-app>
-  </SpeedInsights>
+  <v-app> 
+    <Header />
+    <Rolling />
+  </v-app>
 </template>
 
 <script lang="ts" setup>
@@ -12,8 +10,6 @@ import Header from "@/components/Header/Header.vue";
 import Rolling from "./components/Rolling/Rolling.vue";
 import { computed } from "vue";
 import { useRollingStore } from "./stores/rolling";
-import { SpeedInsights } from "@vercel/speed-insights/vue";
-
 computed(() => localStorage);
 computed(() => useRollingStore());
 </script>
