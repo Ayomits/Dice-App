76e308759d27ce98c54e30aff30c9ed3f0ef4bb5
unknown <nikitakabo74@gmail.com>
2024-12-22 15:08:55 +0300

    feat: vercel SpeedInsights components

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <v-app>
-    <Header />
-    <Rolling />
-  </v-app>
+  <SpeedInsights>
+    <v-app>
+      <Header />
+      <Rolling />
+    </v-app>
+  </SpeedInsights>
 </template>
 
 <script lang="ts" setup>
@@ -10,6 +12,7 @@ import Header from "@/components/Header/Header.vue";
 import Rolling from "./components/Rolling/Rolling.vue";
 import { computed } from "vue";
 import { useRollingStore } from "./stores/rolling";
+import { SpeedInsights } from "@vercel/speed-insights/vue";
 
 computed(() => localStorage);
 computed(() => useRollingStore());
